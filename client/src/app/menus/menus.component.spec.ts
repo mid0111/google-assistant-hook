7c239dfd8eac1c26f493c74cc36608c94f90d2ec
mid0111 <m.tajima0111@gmail.com>
2018-01-10 14:54:49 +0900

    Page Object を利用するように変更

--- client/src/app/menus/menus.component.spec.ts
+++ client/src/app/menus/menus.component.spec.ts
@@ -6,15 +6,12 @@ import { MenusComponent } from './menus.component';
 import { ProfileService } from '../profile.service';
 import { of } from 'rxjs/observable/of';
 
+let component: MenusComponent;
+let fixture: ComponentFixture<MenusComponent>;
+let profileService: ProfileService;
+let page: Page;
+
 describe('MenusComponent', () => {
-  let component: MenusComponent;
-  let fixture: ComponentFixture<MenusComponent>;
-  let profileService: ProfileService;
-
-  let title: DebugElement;
-  let menus: DebugElement[];
-  let loginMessage: DebugElement;
-
   const profileServiceStub = {
     getProfile: () => of({ authenticated: false })
   };
@@ -29,65 +26,76 @@ describe('MenusComponent', () => {
       .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MenusComponent);
-    component = fixture.componentInstance;
-
-    profileService = fixture.debugElement.injector.get(ProfileService);
-  });
-
   describe('ログイン前', () => {
 
     beforeEach(async(() => {
-      fixture.detectChanges();
-      fixture.whenStable().then(() => {
-        title = fixture.debugElement.query(By.css('.title'));
-        menus = fixture.debugElement.queryAll(By.css('li'));
-        loginMessage = fixture.debugElement.query(By.css('p'));
+      createComponent({
+        authenticated: false
       });
     }));
 
     it('ログインメッセージが表示されること', () => {
-      expect(title.nativeElement.textContent).toBe('Google Home に話しかけてみましょう');
+      expect(page.title.textContent).toBe('Google Home に話しかけてみましょう');
 
-      expect(menus.length).toBe(3);
+      expect(page.menus.length).toBe(3);
       let i = 0;
-      expect(menus[i++].nativeElement.textContent).toBe('OK Google!! テレビをつけて!');
-      expect(menus[i++].nativeElement.textContent).toBe('OK Google!! テレビを消して!');
-      expect(menus[i++].nativeElement.textContent).toBe('OK Google!! Rebuild fm を流して!');
+      expect(page.menus[i++].textContent).toBe('OK Google!! テレビをつけて!');
+      expect(page.menus[i++].textContent).toBe('OK Google!! テレビを消して!');
+      expect(page.menus[i++].textContent).toBe('OK Google!! Rebuild fm を流して!');
 
-      expect(loginMessage.nativeElement.textContent).toBe('Google Fit を利用する場合は、あらかじめ\n    Google にログイン してください');
+      expect(page.loginMessage.nativeElement.textContent).toBe('Google Fit を利用する場合は、あらかじめ\n    Google にログイン してください');
     });
   });
 
   describe('ログイン後', () => {
 
     beforeEach(async(() => {
-      spyOn(profileService, 'getProfile')
-        .and.returnValue(of({
-          authenticated: true
-        }));
-
-      fixture.detectChanges();
-      fixture.whenStable().then(() => {
-        title = fixture.debugElement.query(By.css('.title'));
-        menus = fixture.debugElement.queryAll(By.css('li'));
-        loginMessage = fixture.debugElement.query(By.css('p'));
+      createComponent({
+        authenticated: true
       });
     }));
 
     it('健康体操の説明が表示されること', () => {
-      expect(title.nativeElement.textContent).toBe('Google Home に話しかけてみましょう');
+      expect(page.title.textContent).toBe('Google Home に話しかけてみましょう');
 
-      expect(menus.length).toBe(5);
+      expect(page.menus.length).toBe(5);
       let i = 0;
-      expect(menus[i++].nativeElement.textContent).toBe('OK Google!! テレビをつけて!');
-      expect(menus[i++].nativeElement.textContent).toBe('OK Google!! テレビを消して!');
-      expect(menus[i++].nativeElement.textContent).toBe('OK Google!! Rebuild fm を流して!');
-      expect(menus[i++].nativeElement.textContent).toBe('ねぇ Google、健康体操を開始して');
-      expect(menus[i++].nativeElement.textContent).toBe('ねぇ Google、健康体操を終了して');
+      expect(page.menus[i++].textContent).toBe('OK Google!! テレビをつけて!');
+      expect(page.menus[i++].textContent).toBe('OK Google!! テレビを消して!');
+      expect(page.menus[i++].textContent).toBe('OK Google!! Rebuild fm を流して!');
+      expect(page.menus[i++].textContent).toBe('ねぇ Google、健康体操を開始して');
+      expect(page.menus[i++].textContent).toBe('ねぇ Google、健康体操を終了して');
 
-      expect(loginMessage).toBeNull();
+      expect(page.loginMessage).toBeNull();
     });
   });
 });
+
+class Page {
+  title: HTMLElement;
+  menus: HTMLElement[];
+  loginMessage: DebugElement;
+
+  addPageElements() {
+    this.title = fixture.debugElement.query(By.css('.title')).nativeElement;
+    this.menus = fixture.debugElement.queryAll(By.css('li'))
+      .map((debugElement) => debugElement.nativeElement);
+    this.loginMessage = fixture.debugElement.query(By.css('p'));
+  }
+}
+
+function createComponent(testProfile) {
+  fixture = TestBed.createComponent(MenusComponent);
+  component = fixture.componentInstance;
+  profileService = fixture.debugElement.injector.get(ProfileService);
+
+  spyOn(profileService, 'getProfile')
+    .and.returnValue(of(testProfile));
+
+  page = new Page();
+
+  fixture.detectChanges();
+  return fixture.whenStable().then(() => {
+    page.addPageElements();
+  });
+}
