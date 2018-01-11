c0bbbcf556fcee7ec4d6ea31652c9a1943f2b129
mid0111 <m.tajima0111@gmail.com>
2018-01-11 17:32:37 +0900

    stream 再生する前に URL チェックを行うように修正

--- server/routes/api/stream.js
+++ server/routes/api/stream.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const googleHome = require('google-home-notifier');
+const request = require('request');
+const HttpStatus = require('http-status');
 
 const Logger = require('../../lib/Logger');
 const config = require('../../config/app.json');
@@ -13,9 +15,23 @@ const logger = new Logger();
 
 router.post('/', function (req, res) {
   const streamUrl = req.body.url;
-  googleHome.play(streamUrl, (notifyRes) => {
-    logger.info(notifyRes);
-    res.status(206).send();
+  request.get(streamUrl, (error, streamResponse) => {
+    if (error) {
+      res.status(503).json({
+        message: error.message || HttpStatus[503]
+      });
+    }
+    if (streamResponse.statusCode >= 400) {
+      res.status(streamResponse.statusCode).json({
+        message: streamResponse.body || HttpStatus[streamResponse.statusCode]
+      });
+      return;
+    }
+    googleHome.play(streamUrl, (notifyRes) => {
+      logger.info(notifyRes);
+      res.status(206).send();
+    });
+
   });
 });
 
