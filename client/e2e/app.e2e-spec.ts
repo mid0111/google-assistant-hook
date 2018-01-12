160d4a3b0117f5ca34fcbe48b4395ee712610e1f
mid0111 <m.tajima0111@gmail.com>
2018-01-12 12:13:35 +0900

    e2e テストのエラーを修正

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('google-assistant-hook App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to Google Assistant Hook!');
+    expect(page.getParagraphText()).toEqual('Google Assistant Hook');
   });
 });
