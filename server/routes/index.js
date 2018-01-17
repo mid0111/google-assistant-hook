5c3764beee6f98c4b7c0c6e3250206de8554c52c
mid0111 <m.tajima0111@gmail.com>
2018-01-17 12:02:18 +0900

    不要なルーティングを削除

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,8 @@
 var express = require('express');
-var GoogleOAuthClient = require('../lib/GoogleOAuthClient');
 
 var router = express.Router();
 
 router.use('/auth', require('./auth'));
-
-router.get('/', function (req, res) {
-  res.render('index', {
-    auth: GoogleOAuthClient.isAuthenticated()
-  });
-});
-
 router.use('/api', require('./api'));
 
 module.exports = router;
