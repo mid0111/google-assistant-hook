390c802371cd86c93165c7602f06b107eaafb4a6
mid0111 <m.tajima0111@gmail.com>
2018-01-22 13:24:52 +0900

    home ページの e2e を Rebuild FM ページ追加に合わせて修正

--- client/e2e/app-home.po.ts
+++ client/e2e/app-home.po.ts
@@ -7,6 +7,10 @@ export class AppHomePage {
   loginMessage: ElementFinder;
   loginLink: ElementFinder;
 
+  rebuildFmTitle: ElementFinder;
+  rebuildFmDescription: ElementFinder;
+  rebuildFmButton: ElementFinder;
+
   streamMusicTitle: ElementFinder;
   streamMusicDescription: ElementFinder;
   streamMusicButton: ElementFinder;
@@ -22,8 +26,12 @@ export class AppHomePage {
     this.loginMessage = element(by.css('app-home .login-message'));
     this.loginLink = this.loginMessage.element(by.css('a'));
 
-    this.streamMusicTitle = element(by.css('app-home app-extensions .card-title'));
-    this.streamMusicDescription = element(by.css('app-home app-extensions .card-text'));
-    this.streamMusicButton = element(by.css('app-home app-extensions .btn'));
+    this.rebuildFmTitle = element.all(by.css('app-home app-extensions .card-title')).get(0);
+    this.rebuildFmDescription = element.all(by.css('app-home app-extensions .card-text')).get(0);
+    this.rebuildFmButton = element.all(by.css('app-home app-extensions .btn')).get(0);
+
+    this.streamMusicTitle = element.all(by.css('app-home app-extensions .card-title')).get(1);
+    this.streamMusicDescription = element.all(by.css('app-home app-extensions .card-text')).get(1);
+    this.streamMusicButton = element.all(by.css('app-home app-extensions .btn')).get(1);
   }
 }
