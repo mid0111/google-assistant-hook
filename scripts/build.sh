424abedafe063fc3eeb0388f782dce29276e9281
mid0111 <m.tajima0111@gmail.com>
2018-01-22 20:06:08 +0900

    yarn インストール時のログ出力を抑止

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ mkdir dist
 
 cp -pr {server,package.json,yarn.lock} dist/
 pushd dist/
-yarn --prod
+yarn install --prod --silent --no-progress
 tar cvfz build.tar.gz ./*
 
 rm -rf {server,package.json,yarn.lock}
