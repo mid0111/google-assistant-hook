576bcd64407e1aecdf9a855a7f0dcf811da90fe2
mid0111 <m.tajima0111@gmail.com>
2018-01-25 17:42:09 +0900

    デバッグ出力を追加

--- scripts/postdeploy.sh
+++ scripts/postdeploy.sh
@@ -4,3 +4,6 @@ npm run build
 
 cp ./server/config/googleClientSecret.json.sample ./server/config/googleClientSecret.json
 cp ./server/config/braviaSecret.json.sample ./server/config/braviaSecret.json
+
+pwd
+ls
