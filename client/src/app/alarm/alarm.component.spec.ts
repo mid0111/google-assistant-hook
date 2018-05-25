62cf22b87a9dbc18bdda6f573c0f678017f13cbe
mid0111 <m.tajima0111@gmail.com>
2018-05-25 12:32:09 +0900

    アラーム一覧ページを作成

--- client/src/app/alarm/alarm.component.spec.ts
+++ client/src/app/alarm/alarm.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AlarmComponent } from './alarm.component';
+import { SharedModule } from '../shared/shared.module';
 
 describe('AlarmComponent', () => {
   let component: AlarmComponent;
@@ -8,6 +9,7 @@ describe('AlarmComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AlarmComponent],
+      imports: [SharedModule],
     })
       .compileComponents();
   }));
--- client/src/app/alarm/alarm.component.ts
+++ client/src/app/alarm/alarm.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlarmComponent implements OnInit {
 
-  constructor() { }
+  title: string;
+  description: string;
+
+  time: string;
+  message: string;
+
+  alarms: {
+    time: string;
+    message: string;
+  }[];
+
+  constructor() {
+    this.title = 'アラーム';
+    this.description = 'アラームの設定をする';
+
+    this.alarms = [{
+      time: '07:00',
+      message: '授乳時間です。',
+    }, {
+      time: '08:25',
+      message: '朝寝開始５分前です。オムツを交換して寝室に移動しましょう。',
+    }, {
+      time: '08:30',
+      message: '朝寝開始時刻です。',
+    }, {
+      time: '10:00',
+      message: '朝寝終了時刻です。',
+    }];
+  }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+  }
 }
