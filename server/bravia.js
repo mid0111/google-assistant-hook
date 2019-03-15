a1d37e9a2b6ea6f88ccb697157c53fb9c9713114
mid0111 <m.tajima0111@gmail.com>
2019-03-15 14:23:47 +0900

    テレビのチャンネル操作を追加

---
+++ server/bravia.js
@@ -0,0 +1,14 @@
+var Bravia = require('bravia');
+
+// Accepts two parameters, IP and PSKKey
+var bravia = new Bravia('192.168.0.2', '80', '0015');
+
+// Retrieves all the available IRCC commands from the TV.
+bravia.getIRCCCodes()
+  .then((commands) => console.log('getIRCCCodes', commands))
+  .catch((error) => console.error(error));
+
+// Queries the volume info.
+bravia.audio.invoke('getVolumeInformation')
+  .then((info) => console.log('getVolumeInformation', info))
+  .catch((error) => console.error(error));
